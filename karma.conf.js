66cbe8d9dd9412baf9592ef9eb752710d059f7e6
vdro <tomasz.faldrowicz@inkubit.com>
2019-04-13 16:18:14 +0200

    ok

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+
 
 module.exports = function (config) {
   config.set({
@@ -13,7 +12,7 @@ module.exports = function (config) {
       require('@angular/cli/plugins/karma')
     ],
     client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false 
     },
     files: [
       { pattern: './src/test.ts', watched: false }
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
 
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
@@ -12,21 +12,15 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
 declare var require: any;
 
-// Prevent Karma from running prematurely.
 __karma__.loaded = function () {};
 
-// First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-// Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
 context.keys().map(context);
-// Finally, start Karma to run the tests.
 __karma__.start();
